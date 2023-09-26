d9f474d8738853b91f41cd0695738162054451af
nilteras <niltonrnascimento@hotmail.com>
2023-09-26 18:30:25 -0300

    fix: aula-04

--- aula-04/src/app.ts
+++ aula-04/src/app.ts
@@ -6,7 +6,4 @@ app.get("/health", (req: Request, res: Response) => {
   res.status(200).send("OK!");
 });
 
-const port = 6000;
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
-})
+export default app;
