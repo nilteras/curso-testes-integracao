9a22c8b97bb3d249f0d3298eedf12491f68aaf49
nilteras <niltonrnascimento@hotmail.com>
2023-09-26 18:02:12 -0300

    feat: aula-04

---
+++ aula-04/tests/tasks.test.ts
@@ -0,0 +1,16 @@
+import supertest from "supertest";
+import app from "./../src/app";
+
+const server = supertest(app)
+
+describe("api", () => {
+
+    it("/health",async () => {
+
+        const result = await server.get('/health')
+        console.log(result)
+
+        const {statusCode } = result;
+        expect(statusCode).toBe(200)
+    })
+})
