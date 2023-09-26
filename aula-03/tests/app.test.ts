4b1b73f468f0df3684e3bcbf8e1d9bec6c3a405e
nilteras <niltonrnascimento@hotmail.com>
2023-09-26 19:17:00 -0300

    fix: aula-03

--- aula-03/tests/app.test.ts
+++ aula-03/tests/app.test.ts
@@ -1,26 +1,21 @@
 //suite de testes
 //agrupamento de testes que fazer sentido ficarm juntos
 
+import calculator from "calculator"
+
 
 describe("Testes calculadora",() => {
     it("return true for sum", () => {
-        const result = (1 + 2)
 
-        expect(result).toBe(3)
+        expect(calculator.sum(1,2)).toBe(3)
     })
     it("return true for sub", () => {
-        const result = (1 - 2)
-
-        expect(result).toBe(-1)
+        expect(calculator.sub(1,2)).toBe(-1)
     })
-    it("return true for multiplicate", () => {
-        const result = (1 * 2)
-
-        expect(result).toBe(2)
+    it("return true for multiplicate", () => {  
+        expect(calculator.mul(1,2)).toBe(2)
     })
     it("return true for division", () => {
-        const result = (2 / 1)
-
-        expect(result).toBe(2)
+        expect(calculator.div(2,1)).toBe(2)
     })
 })
